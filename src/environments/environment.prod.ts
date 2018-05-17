6667be4dd9f0db30919b8161134d06c93456dddf
Amit X. Shukla <Amit.X.Shukla@kp.org>
2018-05-17 13:35:53 -0700

    checking in environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  },
+  emailAPI: 'http://elisheducation.com/contact-form-ecrm.php',
+  database: 'firebase',
+  social: {
+    fblink: 'https://www.facebook.com/elishconsulting',
+    linkedin: 'https://www.linkedin.com/in/ashuklax/',
+    github: 'https://github.com/AmitXShukla',
+    emailId: 'mailto://amit@elishconsulting.com'
+  },
+  socialAuthEnabled: true
+};
